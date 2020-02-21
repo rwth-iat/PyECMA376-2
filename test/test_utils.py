1e2b8287b5cb4f7f3a2934ff79ff1b87686ee818
Michael Thies <mail@mhthies.de>
2020-02-21 09:35:57 +0100

    Add tests

---
+++ test/test_utils.py
@@ -0,0 +1,10 @@
+import unittest
+
+from pyecma376_2.package_model import part_realpath
+
+
+class TestZipReader(unittest.TestCase):
+    def test_part_realpath(self) -> None:
+        self.assertEqual("/word/document.xml", part_realpath("word/document.xml", "/"))
+        self.assertEqual("/word/document.xml", part_realpath("./document.xml", "/word/anotherPart.xml"))
+        self.assertEqual("/document.xml", part_realpath("../document.xml", "/word/anotherPart.xml"))
