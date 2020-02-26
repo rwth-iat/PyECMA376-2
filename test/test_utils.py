8319c1698493fbc367b0c871c3c9831ebdf0c817
Michael Thies <m.thies@plt.rwth-aachen.de>
2020-02-26 17:16:09 +0100

    tests: Fix structure

--- test/test_utils.py
+++ test/test_utils.py
@@ -3,7 +3,7 @@ import unittest
 from pyecma376_2.package_model import part_realpath
 
 
-class TestZipReader(unittest.TestCase):
+class TestUtilFunctions(unittest.TestCase):
     def test_part_realpath(self) -> None:
         self.assertEqual("/word/document.xml", part_realpath("word/document.xml", "/"))
         self.assertEqual("/word/document.xml", part_realpath("./document.xml", "/word/anotherPart.xml"))
