965dd3a16c66aac88e5266e68906e8e21219b86f
Michael Thies <m.thies@plt.rwth-aachen.de>
2020-02-29 16:18:52 +0100

    core_properties: Add license header and docstring

--- pyecma376_2/core_properties.py
+++ pyecma376_2/core_properties.py
@@ -1,3 +1,19 @@
+# Copyright 2020 by Michael Thies
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+"""
+This module provides a Python model of the OPC Core Properties (according to ECMA 376-2, section 11.). All available
+properties (meta attributes) of a package are represented by an object of the `OPCCoreProperties` class. Additionally,
+this module defines some helper functions to parse or serialize OPCCoreProperties objects from/into XML documents.
+"""
+
 import datetime
 import re
 from typing import Optional, List, Tuple, IO, Callable, Any, Union
